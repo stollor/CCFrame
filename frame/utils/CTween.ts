b9715ca4e39d772411f4c33a038eafabc8c18f11
stollor <906094945@qq.com>
2022-06-09 00:11:39 +0800

    页面渐隐渐显效果

--- frame/utils/CTween.ts
+++ frame/utils/CTween.ts
@@ -1,6 +1,18 @@
-import { tween, TweenEasing } from "cc";
+import { tween, TweenEasing ,Node} from "cc";
+import { PageAniType } from "../EnumMgr";
 
 export class CTween{
+
+    public static runPageAni(node:Node,type:PageAniType,cb:Function=null){
+        switch(type){
+          case PageAniType.fadeIn:
+              this.fadeIn(node,0.4,cb);
+              break;
+          case PageAniType.fadeOut:
+              this.fadeOut(node,0.4,cb);
+              break;
+        }
+    }
 
     public static FromTo(time:number,from:any,to:any,updata:Function,easing:TweenEasing ='linear',cb:Function=null){
       var obj={val:from}  
@@ -20,6 +32,26 @@ export class CTween{
        var obj={val:0};  
         tween(obj)
           .delay(time)
+          .call(cb)
+          .start();
+    }
+
+    public static fadeIn(obj:Node,time:number=0.5,cb:Function=null){
+      tween(obj)
+          //@ts-ignore
+          .set({oparity:0})
+          //@ts-ignore
+          .to(time,{oparity:255},{easing:"circIn"})
+          .call(cb)
+          .start();
+    }
+
+    public static fadeOut(obj:Node,time:number=0.5,cb:Function=null){
+      tween(obj)
+          //@ts-ignore
+          .set({oparity:255})
+          //@ts-ignore
+          .to(time,{oparity:0},{easing:"circOut"})
           .call(cb)
           .start();
     }
